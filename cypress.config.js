7e6518484bfd79b4e195e2bb55f992c540da37fd
Amanda Rossi <amanda.rossi@hbsis.com.br>
2022-08-15 18:13:03 -0300

    ajustando projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'qcip3z',
   e2e: {
-    projectId: 'qcip3z',
+    //projectId: 'qcip3z',
     baseUrl: 'https://buger-eats-qa.vercel.app',
     viewportWidth: 1440,
     viewportHeight: 900,
