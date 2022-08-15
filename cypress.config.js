5295d0e26c5dc3bad63e09f8f9cd3060c3395311
Amanda Rossi <amanda.rossi@hbsis.com.br>
2022-08-15 17:42:29 -0300

    ajustando informações para o github actions

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "qcip3z"
+    projectId: "qcip3z",
     baseUrl: 'https://buger-eats-qa.vercel.app',
     viewportWidth: 1440,
     viewportHeight: 900,
